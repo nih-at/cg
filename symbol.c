655f8e3b386b8e8b5ab1ca3fc17bcc5e852c3c51
Dieter Baron <dillo@nih.at>
1998-01-18 05:05:28 +0000

    added rest

--- symbol.c
+++ symbol.c
@@ -1,4 +1,92 @@
+#include "symbol.h"
 
+#define TBLSIZE 2048
+
+struct entry {
+    struct entry *next;
+    symbol s;
+};
+
+struct entry table[TBLSIZE];
+
+int yourhash(char *s);
+
+
+
+symbol
+intern(char *s)
+{
+    int i;
+    struct entry *e;
+    
+    i = yourhash(s);
+
+    if (table[i].symbol) {
+	for (e=&table[i]; e->next; e = e->next) {
+	    if (strcmp(e->symbol, s) == 0)
+		return e->symbol;
+	}
+	if ((e->next=(struct entry *)malloc(sizeof(struct entry))) == NULL)
+	    return NULL;
+
+	if ((e->next->symbol=strdup(s)) == NULL) {
+	    free(e->next);
+	    e->next = NULL;
+	    return NULL;
+	}
+	e=e->next;
+	e->next = NULL;
+	return e->symbol;
+    }
+    else
+	return table[i].symbol = strdup(s);
+}
+
+
+
+symbol
+intern_lower(char *s)
+{
+    char b[8192], *t;
+
+    for (t=b; (*t=tolower(*s)); t++,s++)
+	;
+
+    return intern(b);
+}
+
+
+
+symbol
+intern_caps(char *s)
+{
+    char b[8192], *t;
+    int c, id;
+
+    for (c=1,id=0,t=b; (*t=(c?toupper:tolower)(*s)); t++,s++) {
+	switch (*t) {
+	case '-':
+	    c = 1;
+	    break;
+	case 'I':
+	    if (c)
+		id = 1;
+	    c = 0;
+	    break;
+	case 'd':
+	    if (id)
+		*t = 'D';
+	    id = c = 0;
+	    break;
+	default:
+	    c = 0;
+	}
+    }
+
+    return intern(b);
+}
+
+
 
 int
 yourhash(char *s)
